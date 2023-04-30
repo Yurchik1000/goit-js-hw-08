be0160692b2472a6f16ce49346b66a702cbb7b35
Yurii Lubimov <yurchik1000@gmail.com>
2023-04-30 20:26:16 +0300

    ADD 8 HW

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,45 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const createRenderGallery = renderGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', createRenderGallery);
+
+
+function renderGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class='gallery__item'>
+      <a class='gallery__link' href='${original}'>
+      <img
+        class='gallery__image'
+        src='${preview}'
+        data-source='${original}'
+        alt='${description}'/>
+      </a>
+      </li>`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  caption: true,
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+galleryContainer.addEventListener('click', e => {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;    
+    lightbox.show();
+  }
+})
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+const currentTimeKey = 'videoplayer-current-time';
+
+
+const saveCurrentTime = throttle(() => {
+  player.getCurrentTime(time => {
+    localStorage.setItem(currentTimeKey, time);
+    console.log(currentTimeKey);
+  });
+}, 1000);
+console.log(currentTimeKey);
+
+const restoreCurrentTime = () => {
+  const currentTime = localStorage.getItem(currentTimeKey);
+  if (currentTime) {
+    player.setCurrentTime(currentTime)
+  }
+}
+
+player.on('timeupdate', saveCurrentTime);
+restoreCurrentTime()
